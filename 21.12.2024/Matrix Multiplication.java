945150429050607a9e5b4317678c967a62c19428
subhasiniravikumar <151342817+subhasiniravikumar@users.noreply.github.com>
2024-12-22 13:27:08 +0530

    Create Matrix Multiplication.java

---
+++ 21.12.2024/Matrix Multiplication.java
@@ -0,0 +1,25 @@
+public class MatrixMultiplication {
+    public static void multiplyMatrices(int[][] A, int[][] B, int[][] result) {
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B[0].length; j++) {
+                for (int k = 0; k < A[0].length; k++) {
+                    result[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] A = {{1, 2}, {3, 4}};
+        int[][] B = {{5, 6}, {7, 8}};
+        int[][] result = new int[2][2];
+
+        multiplyMatrices(A, B, result);
+        for (int[] row : result) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
