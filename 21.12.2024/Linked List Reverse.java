8b6be366352455c37c75e8542519653062e53962
subhasiniravikumar <151342817+subhasiniravikumar@users.noreply.github.com>
2024-12-22 13:25:03 +0530

    Update and rename knapsack.py to Linked List Reverse.java

---
+++ 21.12.2024/Linked List Reverse.java
@@ -0,0 +1,36 @@
+public class LinkedListReverse {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node reverseList(Node head) {
+        Node prev = null, current = head, next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+
+        head = reverseList(head);
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");  // 4 3 2 1
+            temp = temp.next;
+        }
+    }
+}
