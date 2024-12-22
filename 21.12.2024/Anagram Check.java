9f3ff6cce5c785885ab60418b44a52e57f5f32b8
subhasiniravikumar <151342817+subhasiniravikumar@users.noreply.github.com>
2024-12-22 13:28:13 +0530

    Create Anagram Check.java

---
+++ 21.12.2024/Anagram Check.java
@@ -0,0 +1,13 @@
+public class AnagramCheck {
+    public static boolean areAnagrams(String str1, String str2) {
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(areAnagrams("listen", "silent"));  // true
+    }
+}
